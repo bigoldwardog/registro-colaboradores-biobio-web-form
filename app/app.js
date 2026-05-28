622f1b293dcb800aa45c8348a0d6477b24d78a52
Ronald Salgado <ronald.salgado.aninir@gmail.com>
2026-05-27 22:49:24 -0400

    JS: constantes asignadas con los valores del formulario.

--- app/app.js
+++ app/app.js
@@ -5,4 +5,9 @@ const formulario = document.getElementById("formulario");
 // añadido evento al formulario, previene la recarga de la página automáticamente
 formulario.addEventListener("submit", function (event) {
     event.preventDefault();
+    // constantes de cada campo perteneciente al formulario con .getElementById, se obtiene el valor del campo con .value y se hace un "strip" del contenido quitando espacios al inicio y al final del string con .trim()
+    const nombre = document.getElementById("nombre").value.trim();
+    const apellido = document.getElementById("apellido").value.trim();
+    const cargo = document.getElementById("cargo").value.trim();
+    const correo = document.getElementById("correo").value.trim();
 })
