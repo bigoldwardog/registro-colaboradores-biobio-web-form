8defe8978bd009a69f9aeaa780ef05685edfc126
Ronald Salgado <ronald.salgado.aninir@gmail.com>
2026-05-27 23:12:58 -0400

    JS: funciones reutilizables

--- app/app.js
+++ app/app.js
@@ -5,6 +5,8 @@ const formulario = document.getElementById("formulario");
 // añadido evento al formulario, previene la recarga de la página automáticamente
 formulario.addEventListener("submit", function (event) {
     event.preventDefault();
+
+    limpiarErrores();
     // constantes de cada campo perteneciente al formulario con .getElementById, se obtiene el valor del campo con .value y se hace un "strip" del contenido quitando espacios al inicio y al final del string con .trim()
     const nombre = document.getElementById("nombre").value.trim();
     const apellido = document.getElementById("apellido").value.trim();
@@ -13,6 +15,7 @@ formulario.addEventListener("submit", function (event) {
 
     let formularioValido = true;
 
+    // se valida que cada campo no esté vacío
     if (!validarCampoVacio(nombre)) {
         mostrarError("nombre", "El nombre es obligatorio.");
         formularioValido = false;
@@ -31,29 +34,41 @@ formulario.addEventListener("submit", function (event) {
     if (!validarCampoVacio(correo)) {
         mostrarError("correo", "El correo es obligatorio.");
         formularioValido = false;
-    } else if (!validarCorreo(correo)) {
+    } else if (!validarCorreo(correo)) {  // si el correo no está vacío, se valida el formato y dominio
         mostrarError(
             "correo",
             "Debes ingresar un correo válido con dominio @empresa.cl"
         );
         formularioValido = false;
     }
+    // si todas las validaciones fueron se muestra el mensaje
     if (formularioValido) {
         alert("Formulario enviado correctamente.");
         formulario.reset();
     }
 });
 
+
 function validarCampoVacio(valor) {
     return valor !== "";
 }
 
+//expresión regular para verificar que el correo tenga un string + dominio obligatorio @empresa.cl
 function validarCorreo(correo) {
     const regex = /^[a-zA-Z0-9._%+-]+@empresa\.cl$/;
     return regex.test(correo);
 }
 
+// función para mostrar un error en el BLOCK añadido en css
 function mostrarError(campo, mensaje) {
     const elementoError = document.getElementById(`error-${campo}`);
     elementoError.textContent = mensaje;
+}
+
+function limpiarErrores() {
+    const errores = document.querySelectorAll(".error");
+
+    errores.forEach(error => {
+        error.textContent = "";
+    });
 }
